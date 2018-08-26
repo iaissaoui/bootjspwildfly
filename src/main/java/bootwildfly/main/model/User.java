fb96a9aaa85a2292a066b8acb2aa23be623e0c7f
ia <ia@VI>
2018-08-26 18:44:20 +0100

    restore JPA test

--- src/main/java/bootwildfly/main/model/User.java
+++ src/main/java/bootwildfly/main/model/User.java
@@ -1,14 +1,20 @@
 package bootwildfly.main.model;
 
-//@Entity
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-//	private Long id;
-//	@Column(nullable = false)
-//	private String fn;
-//	@Column(nullable = false)
-//	private String ln;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String fn;
+	@Column(nullable = false)
+	private String ln;
 
 }
