97eb60bf490cadab4359e065d9fd59e99fac7afe
aissa <aissa@192.168.1.10>
2018-09-09 22:38:07 +0100

    app deploying twice issue

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -25,5 +25,7 @@ public class Application extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
+	
+	
 }
  
